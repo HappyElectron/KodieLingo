73cd978ac7f02ebd01c05a7cde69df3cbfe14d56
HappyElectron <90774347+HappyElectron@users.noreply.github.com>
2023-08-22 14:24:28 +1000

    Update for class

--- KodieLingo/Model/User.cs
+++ KodieLingo/Model/User.cs
@@ -13,9 +13,9 @@
         public int LongestStreak { get; set; } = 0;
         public int WeeklyKB { get; set; } = 0;
         public int LifetimeKB { get; set; } = 0;
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
 
         public bool AllowFriendRequests = true;
         
