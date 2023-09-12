5ebdba1e25b60f2d1823ceb2dd723193b743c46e
HappyElectron <90774347+HappyElectron@users.noreply.github.com>
2023-09-12 11:44:30 +1000

    Updated User Data
    Updated User data for Henry. There are also in-progress updates on the friends page, that are non-functional.

--- KodieLingo/Services/ClientStateService.cs
+++ KodieLingo/Services/ClientStateService.cs
@@ -1,4 +1,6 @@
 ﻿using KodieLingo.Model;
+using KodieLingo.Data;
+using Microsoft.EntityFrameworkCore;
 
 // Maintaining client state through a scoped service
 // Yes, it's unsecure, no, I do not care
@@ -11,7 +13,21 @@ namespace KodieLingo.Services
 	public class ClientStateService
 	{
 		public User? User { get; set; }
-
 		public bool OpenNavMenu { get; set; } = true;
+        private DatabaseContext? db { get; set; }
+
+
+        public ClientStateService(DatabaseContext context)
+		{
+			db = context;
+		}
+		public User GetUser(string email)
+		{
+			return db.Users.Where(x => x.Email == email)
+            .Include(b => b.Friend)
+            .Include(b => b.FriendReqIncoming)
+            .Include(b => b.FriendReqOutgoing)
+            .FirstOrDefault();
+        }
 	}
 }
